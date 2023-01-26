8b86578637ac33e3aefcf5cefddfe5165d0de17d
Rasmus Rosendahl-Kaa <github.q2pcy@simplelogin.com>
2023-01-26 15:37:08 +0100

    IT ALL WORKS NOW
    Co-authored-by: PenguDance <PenguDance@users.noreply.github.com>

--- sketch.js
+++ sketch.js
@@ -7,10 +7,13 @@ let P,
   occupied = false,
   s,
   t,
+  fy,
+  fx,
   q = 0,
   dw = 0,
   db = 0,
-  pieces = [];
+  pieces = [],
+  pColour = "Black";
 let pmx, pmy, mx, my;
 function setup() {
   P = size / n;
@@ -44,6 +47,16 @@ function setup() {
   pq = q;
   for (let i = pq; i < 4 + pq; i++) {
     pieces[i] = new Rook(i);
+    q++;
+  }
+  pq = q;
+  for (let i = pq; i < 4 + pq; i++) {
+    pieces[i] = new Bishop(i);
+    q++;
+  }
+  pq = q;
+  for (let i = pq; i < 2 + pq; i++) {
+    pieces[i] = new Queen(i);
     q++;
   }
   pq = q;
@@ -137,6 +150,31 @@ class Rook {
       this.img = createImg("assets/Black/Rook.png");
     }
     this.type = "Rook";
+    this.hasMoved = false;
+    this.img.size(P, P);
+    this.img.position(this.x, this.y);
+  }
+  update(x, y) {
+    this.hasMoved = true;
+    this.x = x;
+    this.y = y;
+    this.img.position(this.x, this.y);
+  }
+}
+class Bishop {
+  constructor(i) {
+    if (i <= pq + 1) {
+      this.x = 2 * P + (i - pq) * 3 * P;
+      this.y = (n - 1) * P;
+      this.colour = "White";
+      this.img = createImg("assets/White/Bishop.png");
+    } else {
+      this.x = 2 * P + (i - pq - 2) * 3 * P;
+      this.y = 0;
+      this.colour = "Black";
+      this.img = createImg("assets/Black/Bishop.png");
+    }
+    this.type = "Bishop";
     this.img.size(P, P);
     this.img.position(this.x, this.y);
   }
@@ -146,7 +184,30 @@ class Rook {
     this.img.position(this.x, this.y);
   }
 }
-
+class Queen {
+  constructor(i) {
+    if (i == pq) {
+      this.x = 3 * P;
+      this.y = (n - 1) * P;
+      this.colour = "White";
+      this.img = createImg("assets/White/Queen.png");
+    } else {
+      this.x = 3 * P;
+      this.y = 0;
+      this.colour = "Black";
+      this.img = createImg("assets/Black/Queen.png");
+    }
+    this.type = "Queen";
+    this.img.size(P, P);
+    this.img.position(this.x, this.y);
+  }
+  update(x, y) {
+    this.x = x;
+    this.y = y;
+    this.hasMoved = true;
+    this.img.position(this.x, this.y);
+  }
+}
 function mouseReleased() {
   if (mouseX >= 0 && mouseX <= size && mouseY >= 0 && mouseY <= size) {
     pmx = mouseX - (mouseX % P);
@@ -157,14 +218,178 @@ function mouseReleased() {
     selected = null;
     s = null;
     t = null;
-    print("Fuck dig cunt, du er udenfor brættet");
   }
+}
+
+function pawnMove(colour) {
+  if (colour === "White") {
+    var move = 1;
+  } else {
+    var move = -1;
+  }
+
+  if (pmy + P * move == pieces[s].y) {
+    if (target == null) {
+      if (pmx == pieces[s].x) {
+        pieces[s].update(pmx, pmy);
+        pColour = pieces[s].colour;
+      }
+    } else if (target.colour != colour) {
+      if (pmx + P == pieces[s].x) {
+        pieces[s].update(pmx, pmy);
+        removeTarget();
+        pColour = pieces[s].colour;
+      } else if (pmx - P == pieces[s].x) {
+        pieces[s].update(pmx, pmy);
+        removeTarget();
+        pColour = pieces[s].colour;
+      }
+    }
+  } else if (pieces[s].hasMoved == false && pmy + P * move * 2 == pieces[s].y) {
+    if (checkLine(pmx, pmy) && target == null) {
+      pieces[s].update(pmx, pmy);
+      removeTarget();
+      pColour = pieces[s].colour;
+    }
+  }
+}
+function knightMove(colour) {
+  if (target == null || target.colour != colour) {
+    if (pmx + 2 * P == selected.x || pmx - 2 * P == selected.x) {
+      if (pmy + P == selected.y || pmy - P == selected.y) {
+        pieces[s].update(pmx, pmy);
+        removeTarget();
+        pColour = pieces[s].colour;
+      }
+    } else if (pmy + 2 * P == selected.y || pmy - 2 * P == selected.y) {
+      if (pmx + P == selected.x || pmx - P == selected.x) {
+        pieces[s].update(pmx, pmy);
+        removeTarget();
+        pColour = pieces[s].colour;
+      }
+    }
+  }
+}
+function rookMove(colour) {
+  if (target == null || target.colour != colour) {
+    if (
+      (pmx != selected.x && pmy == selected.y) ||
+      (pmy != selected.y && pmx == selected.x)
+    ) {
+      if (checkLine(pmx, pmy)) {
+        pieces[s].update(pmx, pmy);
+        removeTarget();
+        pColour = pieces[s].colour;
+      }
+    }
+  }
+}
+
+function bishopMove(colour) {
+  if (target == null || target.colour != colour) {
+    fx = pmx - selected.x;
+    fy = pmy - selected.y;
+    if (abs(fx) == abs(fy)) {
+      if (checkLine(pmx, pmy)) {
+        pieces[s].update(pmx, pmy);
+        removeTarget();
+        pColour = pieces[s].colour;
+      }
+    }
+  }
+}
+function queenMove(colour) {
+  if (target == null || target.colour != colour) {
+    fx = pmx - selected.x;
+    fy = pmy - selected.y;
+    if (abs(fx) == abs(fy) || fx == 0 || fy == 0) {
+      if (checkLine(pmx, pmy)) {
+        pieces[s].update(pmx, pmy);
+        removeTarget();
+        pColour = pieces[s].colour;
+      }
+    }
+  }
+}
+function kingMove(colour) {
+  if (target == null || target.colour != colour) {
+    if (abs(pmx - selected.x) == 2 * P) {
+      if (pieces[s].hasMoved == false) {
+        if (pmx - selected.x > 0) {
+          for (let i = 0; i < pieces.length; i++) {
+            if (
+              pieces[i].x == 7 * P &&
+              pieces[i].y == pieces[s].y &&
+              pieces[i].hasMoved == false
+            ) {
+              if (checkLine(7 * P, pieces[s].y)) {
+                pieces[s].update(pmx, pmy);
+                pieces[i].update(pmx - P, pmy);
+                pColour = pieces[s].colour;
+                return;
+              }
+            }
+          }
+        } else if (pmx - selected.x < 0) {
+          for (let i = 0; i < pieces.length; i++) {
+            if (
+              pieces[i].x == 0 * P &&
+              pieces[i].y == pieces[s].y &&
+              pieces[i].hasMoved == false
+            ) {
+              if (checkLine(0 * P, pieces[s].y)) {
+                pieces[s].update(pmx, pmy);
+                pieces[i].update(pmx + P, pmy);
+                pColour = pieces[s].colour;
+                return;
+              }
+            }
+          }
+        }
+      }
+    } else if (abs(pmx - selected.x) <= P && abs(pmy - selected.y) <= P) {
+      pColour = pieces[s].colour;
+      pieces[s].update(pmx, pmy);
+      removeTarget();
+    }
+  }
+}
+
+function checkLine(checkx, checky) {
+  while (checkx != selected.x || checky != selected.y) {
+    if (pmx > selected.x) {
+      checkx -= P;
+    } else if (pmx < selected.x) {
+      checkx += P;
+    }
+    if (pmy > selected.y) {
+      checky -= P;
+    } else if (pmy < selected.y) {
+      checky += P;
+    }
+    if (checkx == selected.x && checky == selected.y) {
+      break;
+    }
+    if (checkIfOccupied(checkx, checky)) {
+      occupied = false;
+      return false;
+    }
+  }
+  return true;
+}
+function checkIfOccupied(x, y) {
+  for (let i = 0; i < pieces.length; i++) {
+    if (pieces[i].x == x && pieces[i].y == y) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function checkSquare(x, y) {
   if (selected == null) {
     for (let i = 0; i < pieces.length; i++) {
-      if (pieces[i].x == x && pieces[i].y == y) {
+      if (pieces[i].x == x && pieces[i].y == y && pColour != pieces[i].colour) {
         selected = pieces[i];
         s = i;
       }
@@ -179,6 +404,10 @@ function checkSquare(x, y) {
       knightMove(selected.colour);
     } else if (selected.type === "Rook") {
       rookMove(selected.colour);
+    } else if (selected.type === "Bishop") {
+      bishopMove(selected.colour);
+    } else if (selected.type == "Queen") {
+      queenMove(selected.colour);
     }
     selected = null;
     target = null;
@@ -186,6 +415,7 @@ function checkSquare(x, y) {
     s = null;
   }
 }
+
 function checkTarget() {
   for (let i = 0; i < pieces.length; i++) {
     if (pieces[i].x == pmx && pieces[i].y == pmy) {
@@ -197,7 +427,6 @@ function checkTarget() {
 }
 function removeTarget() {
   if (target != null) {
-    print("Fjerner mål");
     if (pieces[t].colour === "Black") {
       if (db < 8) {
         pieces[t].update(size, (db * P) / 2);
@@ -215,99 +444,5 @@ function removeTarget() {
     }
     pieces[t].img.size(P / 2, P / 2);
     pieces[t].isDead = true;
-  }
-}
-function pawnMove(colour) {
-  if (colour === "White") {
-    var move = 1;
-  } else {
-    var move = -1;
-  }
-  if (pmy + P * move == pieces[s].y) {
-    if (target == null) {
-      if (pmx == pieces[s].x) {
-        pieces[s].update(pmx, pmy);
-      }
-    } else if (target.colour != colour) {
-      if (pmx + P == pieces[s].x) {
-        pieces[s].update(pmx, pmy);
-        removeTarget();
-      } else if (pmx - P == pieces[s].x) {
-        pieces[s].update(pmx, pmy);
-        removeTarget();
-      }
-    }
-  }
-}
-function kingMove(colour) {
-  if (target == null || target.colour != colour) {
-    if (abs(pmx - selected.x) <= P && abs(pmy - selected.y) <= P) {
-      pieces[s].update(pmx, pmy);
-      removeTarget();
-    }
-  }
-}
-
-function knightMove(colour) {
-  if (target == null || target.colour != colour) {
-    if (pmx + 2 * P == selected.x || pmx - 2 * P == selected.x) {
-      if (pmy + P == selected.y || pmy - P == selected.y) {
-        pieces[s].update(pmx, pmy);
-        removeTarget();
-      }
-    } else if (pmy + 2 * P == selected.y || pmy - 2 * P == selected.y) {
-      if (pmx + P == selected.x || pmx - P == selected.x) {
-        pieces[s].update(pmx, pmy);
-        removeTarget();
-      }
-    }
-  }
-}
-function rookMove(colour) {
-  if (target == null || target.colour != colour) {
-    if (
-      (pmx != selected.x && pmy == selected.y) ||
-      (pmy != selected.y && pmx == selected.x)
-    ) {
-      if (checkLine()) {
-        print(checkLine());
-        pieces[s].update(pmx, pmy);
-        removeTarget();
-      }
-    }
-  }
-}
-
-function checkLine() {
-  let checkx = pmx;
-  let checky = pmy;
-  print(checkx, checky, selected.x, selected.y);
-  while (checkx != selected.x && checky != selected.y) {
-    print("checkLine");
-    if (pmx > selected.x) {
-      checkx -= P;
-    } else if (pmx < selected.x) {
-      checkx += P;
-    }
-    if (pmy > selected.y) {
-      checky -= P;
-    } else if (pmy < selected.y) {
-      checky += P;
-    }
-    print(checkx, checky, selected.x, selected.y);
-    if (checkIfOccupied(checkx, checky)) {
-      occupied = false;
-      return false;
-    }
-  }
-}
-
-function checkIfOccupied(x, y) {
-  print("Tjekker felt");
-  for (let i = 0; i < pieces.length; i++) {
-    print(pieces[i].x, pieces[i].y, x, y);
-    if (pieces[i].x == x && pieces[i].y == y) {
-      occupied = true;
-    }
   }
 }
